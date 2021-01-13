980a47cbe55da051f4b90ac666e1ab9a8f039225
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-01-13 16:03:38 -0600

    Adding Spark RDDs notebook

---
+++ notebooks/Users/eric.montufar@gmail.com/Spark_Essentials/Spark_RDDs.scala
@@ -0,0 +1,117 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## Spark RDDs
+// MAGIC 
+// MAGIC Distributed typed collections of JvM objects. Lowest level API in Spark. 
+// MAGIC They can be highly optimized. But they require you to know the spark internals very well and everything has to be manually tuned. 
+// MAGIC 
+// MAGIC Dataframes are auto-optimized by Spark. 
+
+// COMMAND ----------
+
+// the SparkContext is the entry point for low-level APIs, including RDDs
+val sc = spark.sparkContext
+
+// 1 - parallelize an existing collection
+val numbers = 1 to 1000000
+val numbersRDD = sc.parallelize(numbers)
+
+// COMMAND ----------
+
+// 2 - reading from files
+case class StockValue(symbol: String, date: String, price: Double)
+def readStocks(filename: String) =
+    Source.fromFile(filename)
+      .getLines()
+      .drop(1)
+      .map(line => line.split(","))
+      .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+      .toList
+
+// COMMAND ----------
+
+ val stocksRDD = sc.parallelize(readStocks("src/main/resources/data/stocks.csv"))
+
+// COMMAND ----------
+
+// 2b - reading from files
+val stocksRDD2 = sc.textFile("src/main/resources/data/stocks.csv")
+    .map(line => line.split(","))
+    .filter(tokens => tokens(0).toUpperCase() == tokens(0))
+    .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+
+// COMMAND ----------
+
+// 3 - read from a DF
+val stocksDF = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("src/main/resources/data/stocks.csv")
+
+// COMMAND ----------
+
+import spark.implicits._
+val stocksDS = stocksDF.as[StockValue]
+val stocksRDD3 = stocksDS.rdd
+
+// COMMAND ----------
+
+// RDD -> DF
+val numbersDF = numbersRDD.toDF("numbers") // you lose the type info
+
+// COMMAND ----------
+
+// RDD -> DS
+val numbersDS = spark.createDataset(numbersRDD) // you get to keep type info
+
+// COMMAND ----------
+
+// Transformations
+
+// distinct
+val msftRDD = stocksRDD.filter(_.symbol == "MSFT") // lazy transformation
+val msCount = msftRDD.count() // eager ACTION
+
+// COMMAND ----------
+
+// counting
+val companyNamesRDD = stocksRDD.map(_.symbol).distinct() // also lazy
+
+// COMMAND ----------
+
+// min and max
+implicit val stockOrdering: Ordering[StockValue] =
+Ordering.fromLessThan[StockValue]((sa: StockValue, sb: StockValue) => sa.price < sb.price)
+val minMsft = msftRDD.min() // action
+
+// COMMAND ----------
+
+// reduce
+numbersRDD.reduce(_ + _)
+
+// COMMAND ----------
+
+// grouping
+val groupedStocksRDD = stocksRDD.groupBy(_.symbol)
+// ^^ very expensive
+
+// COMMAND ----------
+
+// Partitioning
+val repartitionedStocksRDD = stocksRDD.repartition(30)
+  repartitionedStocksRDD.toDF.write
+    .mode(SaveMode.Overwrite)
+    .parquet("src/main/resources/data/stocks30")
+  /*
+    Repartitioning is EXPENSIVE. Involves Shuffling.
+    Best practice: partition EARLY, then process that.
+    Size of a partition 10-100MB.
+   */
+
+// COMMAND ----------
+
+// coalesce
+val coalescedRDD = repartitionedStocksRDD.coalesce(15) // does NOT involve shuffling
+  coalescedRDD.toDF.write
+    .mode(SaveMode.Overwrite)
+    .parquet("src/main/resources/data/stocks15")
