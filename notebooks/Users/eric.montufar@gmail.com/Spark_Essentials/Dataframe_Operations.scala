053dfd1c6a97b42c69d7e022b30d8532f7eba51f
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-01-26 15:58:02 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/Spark_Essentials/Dataframe_Operations.scala
+++ notebooks/Users/eric.montufar@gmail.com/Spark_Essentials/Dataframe_Operations.scala
@@ -244,7 +244,19 @@ guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), expr("array_con
 
 // COMMAND ----------
 
+// collection of tupples
 
+val data = Seq(("James, A, Smith","2018","M",3000),
+    ("Michael, Rose, Jones","2010","M",4000),
+    ("Robert,K,Williams","2010","M",4000),
+    ("Maria,Anne,Jones","2005","F",4000),
+    ("Jen,Mary,Brown","2010","",-1)
+  )
+
+  import spark.sqlContext.implicits._
+  val df = data.toDF("name","dob_year","gender","salary")
+  df.printSchema()
+  df.show(false)
 
 // COMMAND ----------
 
