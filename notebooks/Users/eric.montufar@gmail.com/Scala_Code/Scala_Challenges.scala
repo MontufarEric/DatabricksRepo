aab1e284e2a8b546cf935a150f9af1210f33781d
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-02-02 13:31:17 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
+++ notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
@@ -22,7 +22,20 @@ def restock(itemcount: Seq[Int], target: Int): Int = {
 
 // COMMAND ----------
 
+import scala.collection.mutable.ListBuffer
 
+def comparatorValue(a: Seq[Int], b: Seq[Int], d: Int ){
+    var c = 0
+    for (i <- a){
+        var f1 = new ListBuffer[Boolean]()
+        for (j <- b){
+             if(math.abs(j-i) > d) f1 += true else f1 += false
+        }
+        if (f1.forall(x => {x == true})) c+=1 else c+=0 
+    }
+    return c
+    
+}
 
 // COMMAND ----------
 
