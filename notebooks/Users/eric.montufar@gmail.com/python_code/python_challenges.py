daabf32cbbbc132e7477308839b34215f235fea4
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-02-08 13:17:39 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/python_code/python_challenges.py
+++ notebooks/Users/eric.montufar@gmail.com/python_code/python_challenges.py
@@ -5,6 +5,11 @@
 
 # COMMAND ----------
 
+# the time in words problem
+# https://www.hackerrank.com/challenges/the-time-in-words/problem
+
+# COMMAND ----------
+
 #!/bin/python3
 
 import math
@@ -87,3 +92,40 @@ if __name__ == '__main__':
 
 # COMMAND ----------
 
+# Greedy florist 
+# https://www.hackerrank.com/challenges/greedy-florist/problem
+
+# COMMAND ----------
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    cost = 0
+    c = sorted(c, reverse=True)
+    for i in range(0, len(c)):
+        cost += (i // k + 1) * c[i]
+    return cost
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
