ea9001108f07e9dcf84b323fcd446553bb7ff6fd
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-02-18 11:11:36 -0600

    adding spark tunning notebook

---
+++ notebooks/Users/eric.montufar@gmail.com/Spark_Tunning/spark_joins.scala
@@ -0,0 +1,28 @@
+// Databricks notebook source
+// MAGIC %md 
+// MAGIC ## Broadcast Join
+
+// COMMAND ----------
+
+val peopleDF = Seq(
+  ("andrea", "medellin"),
+  ("rodolfo", "medellin"),
+  ("abdul", "bangalore")
+).toDF("first_name", "city")
+
+peopleDF.show()
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
