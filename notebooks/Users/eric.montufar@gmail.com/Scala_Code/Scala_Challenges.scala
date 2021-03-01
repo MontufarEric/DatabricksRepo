f41116fb6f4482d7c79dadaa6dbfd2a2d363c52c
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-03-01 12:56:07 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
+++ notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
@@ -55,11 +55,55 @@ def comparatorValue1(a: Seq[Int], b: Seq[Int], d: Int ){
 
 // MAGIC %md 
 // MAGIC ### Binary Search 
-// MAGIC Code for implmenting the Bunary Search Algorithm in Scala 
+// MAGIC Code for implmenting the Bunary Search Algorithm in Scala. This answers the question How would you return the position of an element without using the built-in methods in Scala. 
 
 // COMMAND ----------
 
-
+def RecursiveBinarySearch(arr: Array[Int], 
+                          Element_to_Search: Int) 
+                         (low: Int = 0, 
+                          high: Int = arr.length - 1): Int = 
+{ 
+      
+    // If element not found                                
+    if (low > high)  
+        return -1
+      
+    // Getting the middle element 
+    var middle = low + (high - low) / 2
+      
+    // If element found 
+    if (arr(middle) == Element_to_Search) 
+        return middle 
+      
+    // Searching in the left half 
+    else if (arr(middle) > Element_to_Search) 
+        return RecursiveBinarySearch(arr,  
+               Element_to_Search)(low, middle - 1) 
+      
+    // Searching in the right half 
+    else
+        return RecursiveBinarySearch(arr,  
+               Element_to_Search)(middle + 1, high) 
+} 
+  
+// Creating main function 
+def main(args: Array[String]){ 
+      
+    // Calling the binary search function and 
+    // storing its result in index variable 
+    var index = RecursiveBinarySearch(Array(1, 2, 3, 4, 55,  
+                                            65, 75), 4)(0, 6); 
+      
+    // If value not found  
+    if(index == -1) 
+       print("Not Found") 
+          
+    // Else print the index where  
+    // the value is found 
+    else
+       print("Element found at Index " + index) 
+} 
 
 // COMMAND ----------
 
