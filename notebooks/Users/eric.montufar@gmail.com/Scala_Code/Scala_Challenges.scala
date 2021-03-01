e85f02b488dcf4449996ec5a49ddd895455e1d07
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-03-01 13:00:05 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
+++ notebooks/Users/eric.montufar@gmail.com/Scala_Code/Scala_Challenges.scala
@@ -54,7 +54,7 @@ def comparatorValue1(a: Seq[Int], b: Seq[Int], d: Int ){
 // COMMAND ----------
 
 // MAGIC %md 
-// MAGIC ### Binary Search 
+// MAGIC ### Binary Search (Recursive)
 // MAGIC Code for implmenting the Bunary Search Algorithm in Scala. This answers the question How would you return the position of an element without using the built-in methods in Scala. 
 
 // COMMAND ----------
@@ -111,3 +111,59 @@ def main(args: Array[String]){
 
 // COMMAND ----------
 
+def IterativeBinarySearch(arr: Array[Int],  
+                          Element_to_Search: Int): Int =
+{ 
+      
+    // Creating start variable to 
+    // point to the first value 
+    var low = 0
+      
+    // Creating end variable to  
+    // point to the last value 
+    var high = arr.length - 1
+      
+    // Finding the value in the  
+    // array iteratively 
+    while (low <= high) 
+    { 
+          
+        // Getting middle element     
+        var middle = low + (high - low) / 2
+          
+        // If element found in the middle index 
+        if (arr(middle) == Element_to_Search) 
+            return middle 
+          
+        // Searching in the first half 
+        else if (arr(middle) > Element_to_Search) 
+            high = middle - 1
+          
+        // Searching in the second half   
+        else
+            low = middle + 1
+    } 
+      
+    // If value not found in the  
+    // entire array , return -1  
+    -1
+} 
+  
+// Creating main function 
+def main(args: Array[String]) 
+{ 
+      
+    // Calling the binary search function and 
+    // storing its result in index variable 
+    var index = IterativeBinarySearch(Array(1, 2, 3, 4, 55, 
+                                            65, 75), 65); 
+      
+    // If value not found  
+    if(index == -1) 
+       print("Not Found") 
+          
+    // Else print the index where  
+    // the value is found 
+    else
+       print("Element found at Index " + index) 
+} 
