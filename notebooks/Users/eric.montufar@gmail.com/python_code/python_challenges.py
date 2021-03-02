cb297a3f464943934bb6a8c4cb162b0a40726ace
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2021-03-02 08:07:01 -0600

    Update: DeltaTimeTravel notebooks

--- notebooks/Users/eric.montufar@gmail.com/python_code/python_challenges.py
+++ notebooks/Users/eric.montufar@gmail.com/python_code/python_challenges.py
@@ -336,3 +336,48 @@ spark.udf.register("mask_email", StringType())
 
 # COMMAND ----------
 
+# MAGIC %md 
+# MAGIC ## Subset Sum Problem
+
+# COMMAND ----------
+
+# A recursive solution for subset sum
+# problem
+ 
+# Returns true if there is a subset
+# of set[] with sun equal to given sum
+def isSubsetSum(set, n, sum):
+    # Base Cases
+    if (sum == 0):
+        return True
+    if (n == 0):
+        return False
+ 
+    # If last element is greater than
+    # sum, then ignore it
+    if (set[n - 1] > sum):
+        return isSubsetSum(set, n - 1, sum)
+ 
+    # else, check if sum can be obtained
+    # by any of the following
+    # (a) including the last element
+    # (b) excluding the last element
+    return isSubsetSum(
+        set, n-1, sum) or isSubsetSum(
+        set, n-1, sum-set[n-1])
+
+
+# COMMAND ----------
+
+# Driver code
+set = [3, 34, 4, 12, 5, 2]
+sum = 9
+n = len(set)
+if (isSubsetSum(set, n, sum) == True):
+    print("Found a subset with given sum")
+else:
+    print("No subset with given sum")
+
+
+# COMMAND ----------
+
